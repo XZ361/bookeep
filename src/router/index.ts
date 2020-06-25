29b002152e8ebab2ab7e7578b6ed712890e0b360
wangxiaozhan <w2457307263@outlook.com>
2020-06-25 10:30:29 +0800

    第一次提交

---
+++ src/router/index.ts
@@ -0,0 +1,43 @@
+import Labels from '@/views/Labels.vue'
+import Money from '@/views/Money.vue'
+import Statistics from '@/views/Statistics.vue'
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path:'/',
+    redirect:'/money'
+  },
+  {
+    path: '/money',
+    name: 'Money',
+    component: Money
+  },
+  {
+    path:'/labels',
+    name:'Labels',
+    component:Labels
+  },
+  {
+    path:'/statistics',
+    name:'Statistics',
+    component:Statistics
+  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
