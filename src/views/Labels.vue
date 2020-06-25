456b4fe10b30c6a86ed37a8f09ea090c340eafe4
wangxiaozhan <w2457307263@outlook.com>
2020-06-25 16:04:50 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>
+
+     <div class="nav-wrapper">
+        <div class="content">
         Labels.vue
-        <Nav/>
     </div>
+
+        <Nav/>
+     </div>
 </template>
 
 <script>
@@ -12,5 +16,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+    display: flex;
+    border: 1px solid green;
+    flex-direction: column;
+    height:100vh;  
+}
+.content{
+    flex-grow:1;/*让.content尽量占据更多的空间 */
+     overflow: auto;/*让内容区域可滚动 */
+     border: 1px solid blue;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+     <div class="nav-wrapper">
+        <div class="content">
         Statistics.vue
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+    display: flex;
+    border: 1px solid green;
+    flex-direction: column;
+    height:100vh;  
+}
+.content{
+    flex-grow:1;/*让.content尽量占据更多的空间 */
+     overflow: auto;/*让内容区域可滚动 */
+     border: 1px solid blue;
+}
 </style>
