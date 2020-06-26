f14fa277baaf85807ca8d2ddedf73c57e8094266
wangxiaozhan <w2457307263@outlook.com>
2020-06-26 11:51:43 +0800

    创建<Icon/>组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+         <svg class="icon">
+                <use :xlink:href="'#'+name"/>
+        </svg>
+</template>
+
+<script lang="ts" >
+// 将一个目录里面任意后缀的文件，统一全部引入到当前
+const importAll=(requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons',true,/\.svg$/));} catch (error) {console.log(error);}
+
+    export default {
+        name:'Icon',
+        props:['name']
+    }
+</script>
+
+<style lang="scss" scoped>
+.icon {
+       width: 1em; height: 1em;
+       vertical-align: -0.15em;
+       fill: currentColor;
+       overflow: hidden;
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import Icon from "@/components/Icon.vue";
 import Layout from '@/components/Layout.vue';
 import Nav from '@/components/Nav.vue';
 import Vue from 'vue';
@@ -7,9 +8,10 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false
-// 全局引入导航组件,布局组件
+// 全局引入导航组件,布局组件,Icon组件
 Vue.component('Nav',Nav)  
 Vue.component('Layout',Layout)
+Vue.component('Icon',Icon);
 
 new Vue({
   router,
