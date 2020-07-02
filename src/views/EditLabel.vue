40935cee92a6df4ad25b0ed4a09d16ccd2ffdd50
wangxiaozhan <w2457307263@outlook.com>
2020-07-02 21:22:19 +0800

    添加跳转至edit页面的逻辑

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
 <script lang="ts">
     import Vue from 'vue'
     import {Component} from 'vue-property-decorator';
-import tagListModel from '../models/tagListModel';
+    import tagListModel from '../models/tagListModel';
 
     @Component
     export default class EditLabel extends Vue{
