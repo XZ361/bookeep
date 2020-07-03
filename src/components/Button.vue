a66cd81c45bca59937be8b7329289449451d94c5
wangxiaozhan <w2457307263@outlook.com>
2020-07-03 08:46:42 +0800

    添加EditLabel.vue的样式，抽离了Button和FormItem两个组件

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+     <button class="button">
+         <slot/>
+     </button>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.button{
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper{
+        text-align: center;
+        padding: 16px;
+        margin-top: 28px;
+    }
+}
+</style>
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,19 +1,28 @@
 <template>
-    <div>
-        <Icon name="left"/>
-        <span>编辑</span>
-        <Notes filed-name="标签名" placeholder="请输入标签名"/>
-    </div>
+    <Layout>
+        <div class="navBar">
+            <Icon class="leftIcon" name="left"/>
+            <span class="title">编辑标签</span>
+            <span class="rightIcon"></span>
+        </div>
+        <div class="form-wrapper">
+            <FormItem filed-name="标签名" placeholder="请输入标签名"/>
+        </div>
+        <div class="button-wrapper">
+            <Button>删除标签</Button>
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
-    import Notes from '@/components/Money/Notes.vue';
+    import FormItem from '@/components/Money/FormItem.vue';
+    import Button from '@/components/Button.vue';
     import {Component} from 'vue-property-decorator';
     import tagListModel from '../models/tagListModel';
 
     @Component({
-       components:{ Notes}
+       components:{ FormItem,Button}
     })
     export default class EditLabel extends Vue{
         created () {
@@ -31,5 +40,31 @@
 </script>
 
 <style scoped>
+.navBar{
+    text-align: center;
+    font-size: 16px;
+    padding: 12px 16px;
+    background: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    >.title{}
+    > .leftIcon{
+        width: 24px;
+        height: 24px;
+    }
+    > .rightIcon{
 
+    }
+
+}
+.form-wrapper{
+    background: white;
+    margin-top: 8px;
+}
+.button-wrapper{
+    text-align: center;
+    padding: 16px;
+    margin-top: 28px;
+}
 </style>
