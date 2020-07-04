d5f4ca14bd5300248ff75040fb1c948778dbb280
wangxiaozhan <w2457307263@outlook.com>
2020-07-04 09:15:16 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,7 @@
 <script lang="ts">
     import Vue from 'vue'
     import {Component} from 'vue-property-decorator';
-    import tagListModel from '@/models/tagListModel';
+    // import tagListModel from '@/models/tagListModel';
 
     @Component
     export default class Labels extends Vue{
@@ -25,12 +25,7 @@
        createTag(){
         const name=window.prompt('请输入标签名: ');
         if(name){
-             const message= tagListModel.create(name);
-            if(message==='duplicated'){
-                window.alert('标签名重复！')
-             }else if(message==='success'){
-                 window.alert('成功！')
-             }
+            window.createTag(name);
         }
        }
     }
