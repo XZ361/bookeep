20b3b5331fbda16971bed86b41d86ae49cf7eb66
wangxiaozhan <w2457307263@outlook.com>
2020-07-07 11:06:06 +0800

    抽离Tabs.vue，将其用在Money和Statistics页面中，并删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
     <Layout class-prefix="layout">
         <NumberPad  :value.sync="record.amount" @submit="saveRecord"/>
-        <Types :value.sync="record.type"/>
+         <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
         <div class="notes">
           <FormItem filed-name="备注"
                @update:value="onUpdateNotes"
@@ -14,20 +15,22 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
+    import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
   // window.localStorage.setItem('version','0.0.1');
 
 
   @Component({
-    components: {Tags, FormItem, Types, NumberPad},
+    components: {Tabs,Tags, FormItem, NumberPad},
   })
   export default class Money extends Vue{
     get recordList(){
       return this.$store.state.recoreList;
     }
+    recordTypeList = recordTypeList;
     record: RecordItem={
       tags:[],notes:'',type:'-',amount:0
     }
